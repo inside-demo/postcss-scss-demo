f402796ca877d6407206de68fba2d79752cf5aeb
Ivan Demidov <scrum@list.ru>
2018-02-06 17:52:31 +0300

    uptdae

--- postcss-sass.js
+++ postcss-sass.js
@@ -1,9 +1,13 @@
 var fs = require('fs');
 var postcss = require('postcss');
-var syntax = require('postcss-scss');
+
+var plugins = [
+	require('postcss-nested')(),
+	require('postcss-sassy-mixins')()
+]
 
 const scss = fs.readFileSync('src/main.scss', 'utf8');
 
-postcss(require('precss')()).process(scss, { parser: syntax, syntax: syntax }).then(function (result) {
+postcss(plugins).process(scss).then(function (result) {
     console.log(result.content );
 });
